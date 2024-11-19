3a36a267bca39b86f6e48cab3956a3c69319fd73
ElodieGuyard <elodieg29@gmail.com>
2024-11-19 10:27:09 +0100

    MAJ des commentaires, variables, et ajout du type de

---
+++ src/Pokemon.java
@@ -0,0 +1,14 @@
+public class Pokemon {
+
+    //Attributs :
+    public String num;
+    public String nom;
+    public String type;
+
+    //Constructeur:
+    public Pokemon(String num, String nom, String type){
+        this.num = num;
+        this.nom = nom;
+        this.type = type;
+    }
+}
